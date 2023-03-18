99fc387ea62f41e0634ea1a273ea022d75a0e12f
zoop1259 <kdm43211991@gmail.com>
2023-03-18 11:58:09 +0900

    Initial Commit

---
+++ SwiftUILocalization/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUILocalization
+//
+//  Created by 강대민 on 2023/03/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUILocalization/SwiftUILocalizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILocalizationApp.swift
+//  SwiftUILocalization
+//
+//  Created by 강대민 on 2023/03/18.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILocalizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
